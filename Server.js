e8066d5cf224240f9e659368ee68f769acae4252
nithish309 <148962856+nithish309@users.noreply.github.com>
2025-12-19 20:08:12 +0530

    server

--- Server.js
+++ Server.js
@@ -2,35 +2,37 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-
 import adminroute from "./Admindash.js";
 import clubcoordinatorroute from "./Club_coordinator.js";
 import studentroute from "./Student_dashbo.js";
-
 dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "*" }));
+app.use(cors());
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
-const url = process.env.MONGODB_URI;
+const port=process.env.PORT;
+
+const url=process.env.MONGODB_URI;
 
 mongoose.connect(url)
-  .then(() => console.log("Database connected"))
-  .catch(err => console.log("MongoDB error:", err));
-
-// Routes
-app.use("/api/admin", adminroute);
-app.use("/api/faculty", adminroute); 
-app.use("/api/student", studentroute);
-app.use("/api/club", clubcoordinatorroute);
-
-app.get("/", (req, res) => {
-  res.send("Other backend running on Render 🚀");
+.then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.use("/api",adminroute);
+app.use("/api/faculty",adminroute);
+app.use("/api",studentroute);
+app.use("/api",clubcoordinatorroute);
+
+
+app.get("/", (req, res) => {
+  res.send("other backend running on Render 🚀");
+});
+app.listen(port,()=>{
+      console.log(`Server running on port ${port}`);
+
 });
